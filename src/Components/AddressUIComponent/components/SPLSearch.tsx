46589b5379800eb9bd6858483eb326861b3109ef
Ian Salt <ian@isalt.digital>
2023-02-21 11:24:42 +0000

    Getting data from the API ... sweet

--- src/Components/AddressUIComponent/components/SPLSearch.tsx
+++ src/Components/AddressUIComponent/components/SPLSearch.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Pressable } from 'react-native'
 import React, {useState} from 'react'
 
 import styles from '../../../Screens/styles/globalstyles'
@@ -11,34 +11,27 @@ import { LineBreakComponent } from '../../LineBreakComponent'
 const SPLSearch = ({SPLSearchButton, errorText}) => {
   const [searchBy, setSearchBy] = useState<string>('')
 
-  const onSubmit = () => {
-    console.log("Button press sucessful!")
-    /* Validate we have something to do then search */
-    // if (searchBy) { SPLSearchButton(searchBy) }
-    console.log('So our state is: ', searchBy)
-  }
   return (
     <View>
-      <LineBreakComponent />
-      <Text style={styles.H3Bold}>SPLSearch Component</Text>
-      <Text style={styles.H4}>
-        Seach box and button + Get list from server
-      </Text>
       <View style={formStyles.SPLheader}>
         <View style={formStyles.formControl}>
-            <Text style={formStyles.SPLInputLabel}>Search by Postcode:</Text>
+            <Text style={formStyles.SPLInputLabel}>
+              Search by Postcode:
+            </Text>
             <TextInput 
-              placeholder='Enter Postcode'
+              placeholder='Postcode'
               style={formStyles.SPLTextInput}
               onChangeText={newText => setSearchBy(newText)}
               defaultValue={searchBy}
             />
         </View>
-        <Button
+        <Pressable
+          onPress={() => SPLSearchButton(searchBy)}
           style={formStyles.SPLbtn}
-          title='Submit'
-          onPress={() => onSubmit()}
-        />
+        >
+          <Text style={formStyles.SPLButtonText}>Submit</Text>
+        </Pressable>
+
         <Text style={formStyles.SPLerrText}>{errorText}</Text>
       </View>
       <LineBreakComponent />
@@ -62,7 +55,7 @@ const formStyles = StyleSheet.create({
     marginTop: 2,
   },
   SPLInputLabel: {
-    fontSize: 22,
+    fontSize: 18,
     marginBottom: 5,
   },
   SPLTextInput: {
@@ -75,11 +68,20 @@ const formStyles = StyleSheet.create({
     paddingVertical: 7,
   },
   SPLbtn: {
-    borderRadius: 5,
+    alignItems: 'center',
+    alignSelf: 'flex-end',
+    backgroundColor: 'blue',
+    borderRadius: 10,
     //  cursor: 'pointer', for webapp if implemented
-    margin: 5,
+    height: 60,
     paddingHorizontal: 10,
-    paddingVertical: 20,
+    paddingVertical: 15,
+    width: 120,
+  },
+  SPLButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   SPLerrText: {
 
--- src/Components/AddressUIComponent/index.tsx
+++ src/Components/AddressUIComponent/index.tsx
@@ -13,22 +13,18 @@ const AddressUIComponent: React.FC = () => {
   //  My account
   //  https://www.simplylookupconsole.co.uk/A2CustomerAccount/main.aspx?coid=3333347653_30_30_______
   
-  // VARIABLE(S?)Datakey simplypostcode external use for a public facing app
+  // Datakey simplypostcode external public facing app
   const datakey = 'W_5ADFE8B4C9B549918715857FEE048E'
-
   //  State of Address Selection List normally hidden
   const [showSearchList, setShowSearchList] = useState<boolean>(false)
-
   //  Display UI errors - remove in live app (?)
   const [errorTxt, setErrorTxt] = useState<string>('')
-
   //  store for lines to display in List 
   type selectedLinesArray = Array<object>
   const [selectionLines, setSelectionLines] = useState<selectedLinesArray>([])
 
   //  ============ Simply Postcode SEARCH EVENTS ============  //
-
-  // Search button pressed, get list of address for Postcode from Simply Postcode HTTP
+  // Search button pressed, get address list from Simply Postcode HTTP
   const SPLSearchButton = (searchBy: string) => {
     //  searchBy= 'zz99' ...  Shows demo data
     console.log('Search Button for:  ', searchBy)
@@ -42,26 +38,29 @@ const AddressUIComponent: React.FC = () => {
 
   //  Fetch list from server for display
   const SPLFetchList = async (postcode: string) => {
-    const fetchData = (url: string): Promise<void> => {
-      return  fetch(url)
-              .then((data) => {
-                if (data.found === 0) {
-                  console.log('0 data returned: ', data.credits_display_text)
-                  console.log('Errormessage: ', data.errormessage)
-                  setErrorTxt(data.credits_display_text)
-                } 
-                if (data.recordcount === 0) {
+    const fetchData = (url: string) => {
+      return fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.found==0){
+                console.log('0 ' ,data.credits_display_text)
+                console.log('errormessage ' ,data.errormessage)
+                setErrorTxt(data.credits_display_text)
+              } else {
+                if (data.recordcount==0){
                   setErrorTxt('Postcode not found')
                   setShowSearchList(false)
                 }
                 else
                 {
-                  console.log('Found record:  ', data.found)
-                  console.log('Status: ', data.credits_display_text)
+                  console.log('Found record: ',data.found)
+                  console.log('Status: ',data.credits_display_text)
                   setSelectionLines(data.records)
+                  console.log('Data records in state: ', selectionLines)
                   setShowSearchList(true)
                 }
-              })} 
+              }              
+            });}
     const SPLurl = `https://www.simplylookupadmin.co.uk/JSONservice/JSONSearchForAddress.aspx?cross=true&appID=122&datakey=${datakey}&postcode=${postcode}`
     console.log(SPLurl)
     fetchData(SPLurl)
@@ -72,12 +71,10 @@ const AddressUIComponent: React.FC = () => {
     <View>
       <Text style={styles.H2Bold}>Address UI Component</Text>
       <AddressRecProvider>
-
         <SPLSearch 
           SPLSearchButton={SPLSearchButton} 
           errorText={errorTxt} 
         />
-
         {showSearchList &&
           <SPLSelectList 
             datakey={datakey}
@@ -86,11 +83,9 @@ const AddressUIComponent: React.FC = () => {
             setShowSearchList={setShowSearchList}
           />
         }
-
         {!showSearchList &&
           <AddressUI />  //  there is a 1 added in the code what's this?
         }
-
       </AddressRecProvider>
     </View>
   )
