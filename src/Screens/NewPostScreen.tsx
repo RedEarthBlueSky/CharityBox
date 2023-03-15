03d5fc802e2d8ee1689cb696ac3e195cba1aba62
Ian Salt <ian@isalt.digital>
2023-03-15 14:41:51 +0000

    First commit

---
+++ src/Screens/NewPostScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NewPostScreen:React.FC = () => {
+  return (
+    <View>
+      <Text>New Post Screen</Text>
+    </View>
+  )
+}
+
+export { NewPostScreen }
---
+++ src/Screens/index.tsx
@@ -0,0 +1,5 @@
+export * from './HomeScreen'
+export * from './LoginScreen'
+export * from './RegisterScreen'
+export * from './LoginScreen'
+export * from './NewPostScreen'
